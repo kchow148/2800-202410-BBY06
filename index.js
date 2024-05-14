2ea7d2719ffd8eef3296dfe9572b6d767ffa3042
James Tolen <jtolen1@my.bcit.ca>
2024-05-14 12:34:44 -0700

    Created .env file with mongodb secret info

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const saltRounds = 12;
 
 const app = express();
 const port = process.env.PORT || 3000;
-const Joi = require("joi");
+const expireTime = 1 * 60 * 60 * 1000;
 
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_secret = process.env.MONGODB_SESSION_SECRET;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+const Joi = require("joi");
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
