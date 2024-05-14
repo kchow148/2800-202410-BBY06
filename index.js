5357002c6cd7fa4dd77fb6b5c0e8f8ea2976aea3
Ben Liu <benliu2345@gmail.com>
2024-05-14 13:05:22 -0700

    added files to liver server

--- index.js
+++ index.js
@@ -7,9 +7,32 @@ const saltRounds = 12;
 
 const app = express();
 const port = process.env.PORT || 3000;
-const Joi = require("joi");
+const expireTime = 1 * 60 * 60 * 1000;
 
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_secret = process.env.MONGODB_SESSION_SECRET;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+const Joi = require("joi");
+app.use(express.urlencoded({ extended: false }));
+var mongoStore = MongoStore.create({
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}`,
+    crypto: {
+        secret: mongodb_secret
+    }
+});
+const { database } = require('./databaseConnection');
+const userCollection = database.db(mongodb_database).collection('users');
 app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true
+}));
 
 app.get('/', (req, res)=>{
     res.render("index");
