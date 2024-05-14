f2d1619b871ad4ba3d3c33de171b3127b6677305
James Tolen <jtolen1@my.bcit.ca>
2024-05-14 12:54:32 -0700

    Created inout boxes for user to input name, loginID and password

--- index.js
+++ index.js
@@ -17,10 +17,61 @@ const mongodb_database = process.env.MONGODB_DATABASE;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 const Joi = require("joi");
 app.use(express.urlencoded({ extended: false }));
+var mongoStore = MongoStore.create({
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}`,
+    crypto: {
+        secret: mongodb_secret
+    }
+});
+const { database } = require('./databaseConnection');
+const userCollection = database.db(mongodb_database).collection('users');
 app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true
+}));
 
 app.get('/', (req, res)=>{
     res.render("index");
+});
+
+app.get('/createUser', (req, res) => {
+    res.render("createUser");
+});
+
+app.post('/submitUser', async (req, res) => {
+    var username = req.body.username;
+    var loginID = req.body.loginID;
+    var password = req.body.password;
+
+    const schema = Joi.object({
+        loginID: Joi.string().alphanum().max(20).required(),
+        password: Joi.string().max(20).required()
+    });
+
+    const validationResult = schema.validate({ loginID, password });
+    if (validationResult.error) {
+        console.log(validationResult.error);
+        res.redirect("/createUser");
+        return;
+    }
+
+    var hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    await userCollection.insertOne({ username: username, loginID: loginID, password: hashedPassword});
+    console.log("Inserted user");
+
+    // Set session variables for the new user
+    req.session.authenticated = true;
+    req.session.loginID = loginID;
+    req.session.cookie.maxAge = expireTime;
+
+    var html = "successfully created user";
+
+    res.render("home", { html: html });
 });
 
 app.get('*', (req, res)=>{
