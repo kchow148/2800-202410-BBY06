9ab7af3a187e404c234d2c698cf68fbee0c2dcd4
kchow148 <kfc071997@hotmail.com>
2024-05-14 13:15:45 -0700

    icon not displaying

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const saltRounds = 12;
 const app = express();
 const port = process.env.PORT || 3000;
 const Joi = require("joi");
+const favicon = require('serve-favicon');
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.use(favicon(path.join(__dirname + '/public', 'images', 'logo.ico')));
+
 
 app.get('/', (req, res)=>{
     res.render("index");
