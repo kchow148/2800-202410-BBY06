8ce8ca1b341c0eaa3c8469ed9259162e6e09d298
James Tolen <jtolen1@my.bcit.ca>
2024-05-16 16:36:26 -0700

    Displayed the news api onto the summary page and the percent rate

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bcrypt = require("bcrypt");
 const saltRounds = 12;
+const requestPromise = require('request-promise');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,9 +17,13 @@ const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_secret = process.env.MONGODB_SESSION_SECRET;
 const mongodb_database = process.env.MONGODB_DATABASE;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
+const api_key = process.env.API_KEY;
+const api_key_2 = process.env.API_KEY_2
+const api_key_3 = process.env.API_KEY_3
+
 const Joi = require("joi");
-// const favicon = require('serve-favicon');
-// const path = require('path');
+const favicon = require('serve-favicon');
+const path = require('path');
 
 app.use(express.urlencoded({ extended: false }));
 var mongoStore = MongoStore.create({
@@ -29,7 +35,7 @@ var mongoStore = MongoStore.create({
 const { database } = require('./databaseConnection');
 const userCollection = database.db(mongodb_database).collection('users');
 app.set('view engine', 'ejs');
-// app.use(favicon(path.join(__dirname + '/public', 'images', 'logo.ico')));
+app.use(favicon(path.join(__dirname + '/public', 'images', 'logo.ico')));
 
 
 app.use(session({
@@ -68,6 +74,9 @@ app.get('/createUser', (req, res) => {
     res.render("createUser", { html: '' });
 });
 
+app.get('/location', (req, res) => {
+    res.render("location", { html: '' });
+});
 
 app.post('/submitUser', async (req, res) => {
     var username = req.body.username;
@@ -78,7 +87,7 @@ app.post('/submitUser', async (req, res) => {
     if (existingUser) {
         var html = "LoginID already taken. Please choose a different one.";
         return res.render("createUser", { html: html });
-    } 
+    }
 
     const schema = Joi.object({
         loginID: Joi.string().alphanum().max(20).required(),
@@ -94,7 +103,7 @@ app.post('/submitUser', async (req, res) => {
 
     var hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    await userCollection.insertOne({ username: username, loginID: loginID, password: hashedPassword});
+    await userCollection.insertOne({ username: username, loginID: loginID, password: hashedPassword });
     console.log("Inserted user");
 
     // Set session variables for the new user
@@ -157,20 +166,85 @@ app.get('/home', (req, res) => {
 });
 
 
-app.get('/logout', (req,res) => {
+app.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
 })
 
-app.get('/summary', (req, res) => {
-    res.render('summary');
+//Summary Page
+
+// app.get('/summary', (req, res) => {
+//     // Sample data object
+//     const data = {
+//         country: 'Canada',
+//         type: 'CPI',
+//         period: 'Mar 2024',
+//         monthly_rate_pct: 0.63,
+//         yearly_rate_pct: 2.9
+//     };
+
+//     // Render the summary.ejs file and pass the data object
+//     res.render('summary', { data: data });
+// });
+
+
+
+const axios = require('axios');
+
+app.get('/summary', async (req, res) => {
+    try {
+        const country = 'Canada';
+        const options = {
+            url: 'https://api.api-ninjas.com/v1/inflation?country=' + country,
+            headers: {
+                'X-Api-Key': api_key 
+            }
+        };
+
+        const inflationResponse = await requestPromise(options);
+        const inflationDataArray = JSON.parse(inflationResponse);
+        const inflationData = inflationDataArray[0];
+        const newsApiKey = api_key_2;
+        const newsResponse = await axios.get('https://newsapi.org/v2/everything', {
+            params: {
+                q: `inflation ${country}`, 
+                language: 'en',
+                apiKey: newsApiKey,
+                excludeSources: 'reuters'
+            }
+        });
+        let newsArticles = newsResponse.data.articles;
+
+        if (newsArticles.length === 0) {
+            const generalNewsResponse = await axios.get('https://newsapi.org/v2/everything', {
+                params: {
+                    q: 'inflation', 
+                    language: 'en', 
+                    apiKey: newsApiKey,
+                    excludeSources: 'reuters' 
+                }
+            });
+            newsArticles = generalNewsResponse.data.articles;
+        }
+
+        res.render('summary', { inflationData, newsArticles });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred while fetching data');
+    }
 });
 
-app.get('*', (req, res)=>{
+
+
+
+
+
+
+app.get('*', (req, res) => {
     res.status(404);
     res.render("404");
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
